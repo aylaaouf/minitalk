ae7f5ed5bfcea1f5856ebbe9c6d612432123bed3
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-02-28 17:14:11 +0000

    encrypt the message and send it to the server via process ID

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 18:00:13 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/02/27 18:14:28 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2025/02/28 16:58:30 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ void    parser(int ac, char **av)
     }
     while (av[1][i])
     {
-        if (!ft_isdigit(av[1][i]) || pid < 0)
+        if (!ft_isdigit(av[1][i]) || pid <= 0)
         {
             ft_putstr_fd("Error: Invalid PID\n", 2);
             exit(1);
@@ -40,7 +40,36 @@ void    parser(int ac, char **av)
     }
 }
 
+void    encrypt(char c, int pid)
+{
+    int i;
+
+    i = 7;
+    while (i >= 0)
+    {
+        if ((c >> i) & 1)
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+        usleep(500);
+        i++;
+    }
+}
+
 int main(int ac, char **av)
 {
+    int pid;
+    char *msj;
+    int i;
+
+    pid = ft_atoi(av[1]);
+    msj = av[2];
+    i = 0;
     parser(ac, av);
+    while (msj[i])
+    {
+        encrypt(msj[i], pid);
+        i++;
+    }
+    encrypt('\0', pid);
 }
