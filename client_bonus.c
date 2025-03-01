28871a37c5980e49ea18ebb9a97fba804867af54
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-03-01 19:19:26 +0000

    feat: add UNICODE support & log received strings

---
+++ client_bonus.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client_bonus.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/01 16:15:31 by aylaaouf          #+#    #+#             */
+/*   Updated: 2025/03/01 19:09:47 by aylaaouf         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void    parser(int ac, char **av)
+{
+	if (ac != 3)
+	{
+		ft_putstr_fd("Usage: ./client <server_pid> <message>\n", 2);
+		exit(1);
+	}
+	int i;
+	int pid;
+
+	i = 0;
+	pid = ft_atoi(av[1]);
+	if (!av[2][0])
+	{
+		ft_putstr_fd("Error: Message cannot be empty\n", 2);
+		exit(1);
+	}
+	while (av[1][i])
+	{
+		if (!ft_isdigit(av[1][i]) || pid <= 0)
+		{
+			ft_putstr_fd("Error: Invalid PID\n", 2);
+			exit(1);
+		}
+		i++;
+	}
+}
+
+void    encrypt_char(char c, int pid)
+{
+	int i;
+
+	i = 7;
+	while (i >= 0)
+	{
+		if ((c >> i) & 1)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(500);
+		i--;
+	}
+}
+
+void	signal_handler(int sig, siginfo_t *info, void *context)
+{
+	(void)info;
+	(void)context;
+	if (sig == SIGUSR1)
+		ft_putstr_fd("the message has been received\n", 1);
+}
+
+int main(int ac, char **av)
+{
+	int pid;
+	char *msj;
+	int i;
+
+	parser(ac, av);
+	struct sigaction sa;
+
+	sa.sa_sigaction = signal_handler;
+	sa.sa_flags = SA_SIGINFO;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
+	pid = ft_atoi(av[1]);
+	msj = av[2];
+	i = 0;
+	while (msj[i])
+	{
+		encrypt_char(msj[i], pid);
+		i++;
+	}
+	encrypt_char('\0', pid);
+	exit(0);
+}
