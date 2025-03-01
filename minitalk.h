96f548253292689124afe6e57028e2bf6ef11dce
aylaaouf <ayoub.laaoufi@hotmail.com>
2025-03-01 19:14:42 +0000

    add prototypes to header file

--- minitalk.h
+++ minitalk.h
@@ -6,8 +6,21 @@
 /*   By: aylaaouf <aylaaouf@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 13:50:27 by aylaaouf          #+#    #+#             */
-/*   Updated: 2025/02/27 13:51:24 by aylaaouf         ###   ########.fr       */
+/*   Updated: 2025/03/01 16:23:43 by aylaaouf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./libft/libft.h"
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+
+size_t  ft_strlen(const char *s);
+void    ft_putstr_fd(char *s, int fd);
+int     ft_isdigit(int c);
+int	    ft_atoi(const char *str);
+
+#endif
